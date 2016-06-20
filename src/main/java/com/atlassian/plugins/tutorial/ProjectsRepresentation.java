b582f1c0fe387c4ea43b5360f1c6352bbfbe6dc7
Ryan Faith <ricfaith@gmail.com>
2016-06-20 07:30:26 -0700

    Initial version with minimal changes to support JIRA 7.1.7

---
+++ src/main/java/com/atlassian/plugins/tutorial/ProjectsRepresentation.java
@@ -0,0 +1,41 @@
+package com.atlassian.plugins.tutorial;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlElement;
+
+import net.jcip.annotations.Immutable;
+
+/**
+ * JAXB representation of a group of projects.
+ */
+@Immutable
+@SuppressWarnings("UnusedDeclaration")
+@XmlRootElement
+public class ProjectsRepresentation
+{
+    @XmlElement
+    private Collection<ProjectRepresentation> projects;
+
+    // This private constructor isn't used by any code, but JAXB requires any
+    // representation class to have a no-args constructor.
+    private ProjectsRepresentation()
+    {
+        projects = null;
+    }
+
+    /**
+     * Stores the specified {@code Project}s in this representation.
+     * @param projects the projects to store
+     */
+    public ProjectsRepresentation(Iterable<ProjectRepresentation> projects)
+    {
+        this.projects = new HashSet<ProjectRepresentation>();
+        for (ProjectRepresentation representation : projects)
+        {
+            this.projects.add(representation);
+        }
+    }
+}
---
+++ src/main/java/com/atlassian/plugins/tutorial/ProjectsResource.java
@@ -0,0 +1,86 @@
+package com.atlassian.plugins.tutorial;
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.servlet.http.HttpServletRequest;
+
+import com.atlassian.crowd.embedded.api.User;
+import com.atlassian.jira.permission.ProjectPermissions;
+import com.atlassian.jira.project.Project;
+import com.atlassian.jira.security.JiraAuthenticationContext;
+import com.atlassian.jira.security.PermissionManager;
+import com.atlassian.jira.security.Permissions;
+import com.atlassian.jira.user.ApplicationUser;
+import com.atlassian.jira.user.util.UserUtil;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import com.atlassian.sal.api.user.UserManager;
+import com.atlassian.plugins.rest.common.security.AnonymousAllowed;
+
+/**
+ * REST resource that provides a list of projects in JSON format.
+ */
+@Path("/projects")
+public class ProjectsResource
+{
+    private UserManager userManager;
+    private JiraAuthenticationContext authenticationContext;
+    private PermissionManager permissionManager;
+    private UserUtil userUtil;
+
+    /**
+     * Constructor.
+     * @param userManager a SAL object used to find remote usernames in
+     * Atlassian products
+     * @param userUtil a JIRA object to resolve usernames to JIRA's internal
+     * {@code com.opensymphony.os.User} objects
+     * @param permissionManager the JIRA object which manages permissions
+     * for users and projects
+     */
+    public ProjectsResource(
+            @ComponentImport final JiraAuthenticationContext authenticationContext,
+            @ComponentImport final PermissionManager permissionManager) {
+        this.authenticationContext = authenticationContext;
+        this.permissionManager = permissionManager;
+    }
+
+    /**
+     * Returns the list of projects browsable by the user in the specified
+     * request.
+     * @param request the context-injected {@code HttpServletRequest}
+     * @return a {@code Response} with the marshalled projects
+     */
+    @GET
+    @AnonymousAllowed
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public Response getProjects(@Context HttpServletRequest request)
+    {
+        // get the corresponding com.opensymphony.os.User object for
+        // the request
+        final ApplicationUser user = authenticationContext.getUser();
+ 
+        // retrieve all objects for projects this user has permission to browse
+        Collection<Project> projects =
+                permissionManager.getProjects(ProjectPermissions.BROWSE_PROJECTS, user);
+
+        // convert the project objects to ProjectRepresentations
+        Collection<ProjectRepresentation> projectRepresentations =
+                new LinkedList<ProjectRepresentation>();
+        for (Project project : projects)
+        {
+            projectRepresentations.add(new ProjectRepresentation(project));
+        }
+        ProjectsRepresentation allProjects =
+                new ProjectsRepresentation(projectRepresentations);
+
+        // return the project representations. JAXB will handle the conversion
+        // to XML or JSON.
+        return Response.ok(allProjects).build();
+    }
+}
